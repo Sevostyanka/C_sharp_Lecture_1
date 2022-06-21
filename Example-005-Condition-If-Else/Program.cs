e68cd0ff14ec37bcd8206f708ec8f1d71b1e2e48
Ekaterina <sevic_bag@mail.ru>
2022-06-22 02:11:28 +0300

    сделали програмку с разными приветствиями

---
+++ Example-005-Condition-If-Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine ("Ура, вот и ты, дорогая Машуня!");
+}
+else
+{
+    Console.Write("Приветствую, ");
+    Console.Write(username);
+}
